189c8d7aad161780e055702b90f563361112c007
Piotr Wróbel <pwrobel5@gmail.com>
2020-12-05 13:54:40 +0100

    lab 5, all needed versions of neural network

--- 05-miniinceptionnet/model/model.py
+++ 05-miniinceptionnet/model/model.py
@@ -1,19 +1,36 @@
-import tensorflow.keras.models as models
 import tensorflow.keras.layers as layers
+import tensorflow.keras.models as models
+
+X5_PERCENT = 0.2
 
 
-def mini_inception_net_functional(width, height, depth, classes):
+def mini_inception_net(width, height, depth, classes, use_dropout=True, separable_convolutions=False, use_5x5=False,
+                       use_gap=False):
     def conv_module(x, K, kX, kY, stride, chanDim, padding="same"):
-        x = layers.Conv2D(K, (kX, kY), strides=stride, padding=padding)(x)
+        if separable_convolutions:
+            x = layers.SeparableConv2D(K, (kX, kY), strides=stride, padding=padding)(x)
+        else:
+            x = layers.Conv2D(K, (kX, kY), strides=stride, padding=padding)(x)
         x = layers.BatchNormalization(axis=chanDim)(x)
         x = layers.Activation("relu")(x)
 
         return x
 
     def inception_module(x, numK1x1, numK3x3, chanDim):
-        conv_1x1 = conv_module(x, numK1x1, 1, 1, (1, 1), chanDim)
-        conv_3x3 = conv_module(x, numK3x3, 3, 3, (1, 1), chanDim)
-        x = layers.concatenate([conv_1x1, conv_3x3], axis=chanDim)
+        if use_5x5:
+            num_sum = numK1x1 + numK3x3
+            numK5x5 = round(X5_PERCENT * num_sum)
+            numK1x1 = round((1 - X5_PERCENT) * numK1x1)
+            numK3x3 = num_sum - numK5x5 - numK1x1  # to keep filters number constant
+
+            conv_1x1 = conv_module(x, numK1x1, 1, 1, (1, 1), chanDim)
+            conv_3x3 = conv_module(x, numK3x3, 3, 3, (1, 1), chanDim)
+            conv_5x5 = conv_module(x, numK5x5, 5, 5, (1, 1), chanDim)
+            x = layers.concatenate([conv_1x1, conv_3x3, conv_5x5], axis=chanDim)
+        else:
+            conv_1x1 = conv_module(x, numK1x1, 1, 1, (1, 1), chanDim)
+            conv_3x3 = conv_module(x, numK3x3, 3, 3, (1, 1), chanDim)
+            x = layers.concatenate([conv_1x1, conv_3x3], axis=chanDim)
 
         return x
 
@@ -46,8 +63,12 @@ def mini_inception_net_functional(width, height, depth, classes):
     # two Inception modules followed by global POOL and dropout
     x = inception_module(x, 176, 160, chanDim)
     x = inception_module(x, 176, 160, chanDim)
-    x = layers.AveragePooling2D((7, 7))(x)
-    x = layers.Dropout(0.5)(x)
+    if use_gap:
+        x = layers.GlobalAveragePooling2D()(x)
+    else:
+        x = layers.AveragePooling2D((7, 7))(x)
+    if use_dropout:
+        x = layers.Dropout(0.5)(x)
 
     # softmax classifier
     x = layers.Flatten()(x)
--- 05-miniinceptionnet/train.py
+++ 05-miniinceptionnet/train.py
@@ -12,7 +12,12 @@ from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.callbacks import Callback
 
-from .model import *
+from model.model import mini_inception_net
+
+INIT_LR = 1e-2
+BATCH_SIZE = 128
+NUM_EPOCHS = 60
+LABEL_NAMES = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
 
 
 class TimeHistory(Callback):
@@ -26,15 +31,7 @@ class TimeHistory(Callback):
         self.times.append(time.time() - self.epoch_time_start)
 
 
-if __name__ == '__main__':
-    matplotlib.use("Agg")
-    logging.getLogger("tensorflow").setLevel(logging.CRITICAL)
-
-    INIT_LR = 1e-2
-    BATCH_SIZE = 128
-    NUM_EPOCHS = 60
-
-    labelNames = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+def train_model(model, output_suffix):
     print("[INFO] loading CIFAR-10 dataset...")
     ((train_x, train_y), (test_x, test_y)) = cifar10.load_data()
 
@@ -51,8 +48,6 @@ if __name__ == '__main__':
     augmenter = ImageDataGenerator(rotation_range=18, zoom_range=0.15,
                                    width_shift_range=0.2, height_shift_range=-.2, shear_range=0.15,
                                    horizontal_flip=True, fill_mode="nearest")
-
-    model = mini_inception_net_functional(32, 32, 3, len(labelNames))
 
     # initialize the optimizer and compile the model
     optimizer = SGD(lr=INIT_LR, momentum=0.9, decay=INIT_LR / NUM_EPOCHS)
@@ -72,14 +67,14 @@ if __name__ == '__main__':
     )
 
     # save model
-    model.save("miniinception_1")
+    model.save("miniinception_{}".format(output_suffix))
 
     # evaluate the network
     print("[INFO] evaluating network...")
     predictions = model.predict(test_x, batch_size=BATCH_SIZE)
     print(classification_report(test_y.argmax(axis=1),
                                 predictions.argmax(axis=1),
-                                target_names=labelNames))
+                                target_names=LABEL_NAMES))
 
     train_losses = history.history["loss"]
     val_losses = history.history["val_loss"]
@@ -87,7 +82,7 @@ if __name__ == '__main__':
     val_accuracies = history.history["val_accuracy"]
     zipped_results = zip(train_losses, train_accuracies, val_losses, val_accuracies)
 
-    output_accuracy = open("accuracy_1.txt", "w")
+    output_accuracy = open("accuracy_{}.txt".format(output_suffix), "w")
     for index, train_loss, train_accuracy, val_loss, val_accuracy in enumerate(zipped_results):
         output_accuracy.write("{} {} {} {} {}\n".format(index + 1, train_loss, train_accuracy, val_loss, val_accuracy))
     output_accuracy.close()
@@ -107,7 +102,27 @@ if __name__ == '__main__':
     plt.xlabel("Epoka")
     plt.ylabel("Loss/Accuracy")
     plt.legend()
-    plt.savefig("miniinception_1.png")
+    plt.savefig("miniinception_{}.png".format(output_suffix))
 
     average_epoch_time = mean(time_callback.times)
     print("Average epoch training time: {} s".format(average_epoch_time))
+
+
+def main():
+    models = [
+        mini_inception_net(32, 32, 3, len(LABEL_NAMES)),
+        mini_inception_net(32, 32, 3, len(LABEL_NAMES), use_dropout=False),
+        mini_inception_net(32, 32, 3, len(LABEL_NAMES), separable_convolutions=True),
+        mini_inception_net(32, 32, 3, len(LABEL_NAMES), use_5x5=True),
+        mini_inception_net(32, 32, 3, len(LABEL_NAMES), use_gap=True)
+    ]
+
+    for index, model in enumerate(models):
+        train_model(model, str(index + 1))
+
+
+if __name__ == '__main__':
+    matplotlib.use("Agg")
+    logging.getLogger("tensorflow").setLevel(logging.CRITICAL)
+
+    main()
